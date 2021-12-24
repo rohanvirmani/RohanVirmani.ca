a6f653f24502f7f9e8a19604fde25ce995a422c6
Rohan Virmani <rohanster@gmail.com>
2021-12-23 23:03:45 -0500

    Website intial update using nextJS

---
+++ constants/data.js
@@ -0,0 +1,46 @@
+const userData = {
+  githubUsername: "rohanvirmani",
+  name: "Rohan Virmani",
+  designation: "Devops Developer",
+  avatarUrl: "/avatar.png",
+  email: "rohan.virmani@live.com",
+  phone: "",
+  address: "Toronto, Canada.",
+  projects: [
+    {
+      title: "Google",
+      link: "https://google.com",
+      imgUrl: "/tmk.jpg",
+    }
+  ],
+  about: {
+    title:
+      "I'm a devops developer that loves solving problems using technology",
+    description: [
+      `I've been working with dos since I was a kid to play the Lion King and Commander Keen and Doom.`,
+      `But now, I'm a legend. Actually no I am LEGEND.`,
+    ],
+    //currentProject: "Tailwind Master Kit",
+    //currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Ceridian",
+      year: "2019-Present",
+      companyLink: "https://ceridian.com",
+      desc: "Contribute and support our microservice delivery platform to support teams deploying services in Microsoft Azure.",
+    }
+  ],
+  resumeUrl:
+    "https://docs.google.com/document/d/1ClkU6cDxHG1ZA5T7etBLR5Jfakd9osVIM3OAu7se4ZQ/edit?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/itsrohanv",
+    twitter: "https://twitter.com/rohanster",
+    linkedin: "https://www.linkedin.com/in/rohan-virmani-3243069/",
+    github: "https://github.com/rohanvirmani",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
